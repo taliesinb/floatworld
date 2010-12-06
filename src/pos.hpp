ec6868167d147cfb8587e6e3fb48c0f17b61ab71
Taliesin Beynon <taliesinb@gmail.com>
2010-12-06 01:14:59 -0500

    Removed unused FloatPos class. Perhaps I should move Pos class straight into Occupant.hpp

--- src/pos.hpp
+++ src/pos.hpp
@@ -7,8 +7,6 @@
 
 #define PI 3.14159265358979323
 
-class FloatPos;
-
 class Pos
 {
   public:
@@ -16,7 +14,6 @@ class Pos
     short col;
 
     Pos() { }
-    Pos(const FloatPos& pos);
     Pos(const Pos& pos) : row(pos.row), col(pos.col) { }
     Pos(short r, short c) : row(r), col(c) { }
     Pos(int dir)
@@ -55,46 +52,7 @@ class Pos
       return row == pos.row && col == pos.col;
     }
 };
-
-class FloatPos
-{
-  public:
-    float row;
-    float col;
-
-    FloatPos() { }
-    FloatPos(const Pos& pos) : row(pos.row), col(pos.col) { }
-    FloatPos(const FloatPos& pos) : row(pos.row), col(pos.col) { }
-    FloatPos(float r, float c) : row(r), col(c) { }
-    FloatPos(float a) : row(-sin(2 * PI * a)), col(cos(2 * PI * a)) { }
-
-    FloatPos Transpose() { return FloatPos(col, row); }
-
-    #undef MAKE_OP
-    #define MAKE_OP(X) \
-    FloatPos operator X(float f) { return FloatPos(row X f, col X f); } \
-    void operator X##=(float f) { row X##= f; col X##= f; } \
-    FloatPos operator X(FloatPos f) { return FloatPos(row X f.row, col X f.col); } \
-    void operator X##=(FloatPos f) { row X##= f.row; col X##= f.col; }
-    
-    MAKE_OP(+);
-    MAKE_OP(-);
-    MAKE_OP(*);
-    MAKE_OP(/);
-
-    bool operator==(FloatPos pos) {
-      return row == pos.row && col == pos.col;
-    }
-};
-
-inline Pos::Pos(const FloatPos& pos) :
-  row(round(pos.row)), col(round(pos.col))
-{ }
-
 std::ostream& operator<<(std::ostream& os, const Pos& p);
 std::istream& operator>>(std::istream& is, Pos& p);
-
-std::ostream& operator<<(std::ostream& os, const FloatPos& p);
-std::istream& operator>>(std::istream& is, FloatPos& p);
 
 #endif
